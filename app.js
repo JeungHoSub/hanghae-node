0cfab7e354bbd7e575e73f2670d3bca692b99ac1
wnlwjsan@gmail.com <wnlwjsan@gmail.com>
2022-07-25 22:25:18 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+// const mongoose = require("mongoose");
+const express = require("express");
+const connect = require("./schemas");
+const app = express();
+const port = 3000;
+
+connect();
+
+ /* mongoose.connect("mongodb+srv://wlwjsan:!jeunga1125@cluster0.yrmjgdx.mongodb.net/test123?retryWrites=true&w=majority")
+    .then(function(aaa) {
+        console.log("연결성공")
+    })
+    .catch(function(bbb) {
+        console.log(bbb);
+        console.log("연결실패");
+    })
+    
+    */
+
+const goodsRouter = require("./routes/goods");
+
+
+const requestMiddelware = (req, res, next) => {
+    console.log("Request URL:", req.originalUrl, " - ", new Date());
+    next();
+};
+
+app.use(express.json());
+app.use(requestMiddelware);
+
+app.use("/api", [goodsRouter]);
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+});
+
+app.listen(port, () => {
+    console.log(port, "포트로 서버가 켜졌어요!");
+});
+
+// 자바스크립트라는 언어로 코드를 짜고 node.js로 실행을 했지만 express.js라는 도구를 이용해서 만들었어요
---
+++ schemas/index.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+
+const connect = () => {
+   // mongoose.connect("mongodb+srv://wlwjsan:!jeunga1125@cluster0.yrmjgdx.mongodb.net/?retryWrites=true&w=majority").catch((err) => {
+    mongoose.connect("mongodb://127.0.0.1:27017/spa_mall")
+    .catch((err) => {
+        console.error(err);
+    });
+};
+
+module.exports = connect;
+
